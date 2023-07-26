e0133b63decf7eca8d075c8fc908e5114371ab0b
leonardo oliveira <azideiamano20209@gmail.com>
2023-07-26 11:53:58 -0300

    hoje nos fizemos o codigo para excluir e editar

--- controller.js
+++ controller.js
@@ -17,8 +17,35 @@ function salvarUser(){
     let nomeUser = document.getElementById("nomeUser").value;
 
     if(nomeUser){
-        dadoslista.push(nomeUser);
-        console.log(dadoslista);
+        dadoslista.push(nomeUser);  criaLista();
+        document.getElementById("nomeUser").value = '';
+        }else{
+            alert("usuario, por favor preencher o campo nome");
+        }
     }
 
+
+
+// FUNÇÃO PARA CRIAÇÃO DE LISTA NA TABELA
+function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>nomeUsuario</th><th>açôes</th></tr>";
+
+    // LAÇO DE REPETIÇÃO PARA COLOCAR NOMES NA TABELA 
+    for(let i=0; i <= (dadoslista.length-1); i++){
+        tabela += "<tr><td>" + dadoslista[i] + "</td><td><button class='btn btn-success'onclick'editar(this.parentNode.parentNode.rowIndex)'>Editar<button><button class='btn btn-danger'onclick'excluir(this.parentNode.parentNode.rowIndex'>Excluir</button></td></tr>";
+              document.getElementById("tabela"). innerHTML = tabela;
+    }
+
+}
+
+//função para editar nome
+function editar(i){
+    document.getElementById("nomeUser").value = dadoslista[(i - 1)];
+    dadoslista.splice(dadoslista[(i - 1)], 1);
+}
+
+//função excluir 
+function Excluir(i){
+dadoslista.splice(dadoslista[(i - 1)], 1);
+document.getElementById('tabela').deletaRow(i);
 }
